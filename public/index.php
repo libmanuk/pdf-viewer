c17df6114390d26d94890b4c6cd3a651c8d749be
Eric C. Weig <eweig@email.uky.edu>
2025-07-07 14:06:51 -0400

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PDF.js PHP Viewer</title>
+    <link rel="stylesheet" href="pdfjs/web/viewer.css">
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+    </style>
+</head>
+<body>
+    <iframe
+        src="pdfjs/web/viewer.html?file=../sample.pdf"
+        width="100%"
+        height="100%"
+        style="border: none;">
+    </iframe>
+</body>
+</html>
